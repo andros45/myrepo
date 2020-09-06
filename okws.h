9dac9491011e1868f6f08a6b8a0eac6f5a8b4d2b
andros <andjoh333@gmail.com>
2020-09-06 19:06:20 +0200

    New commit 2

--- okws.h
+++ okws.h
@@ -2,3 +2,5 @@
 012938
 12+3+12
 
+woeij
+pweof wefif we wef iowefo iwefoiwef jwef jfeowf jowe fwoei foi wef jiowefåoa fåaåjaeåfwef awepå faf fpawo fawe ffe waef pawe ffw wae fwaef waef waefå awep efwaåf pwae f ef faw effa wpoefwaope fwe a
