e3f86dbc7e47a59dc79451b04a3edb1d6864e465
andros <andjoh333@gmail.com>
2020-09-06 19:08:59 +0200

    Added least folder

---
+++ least/files.h
@@ -0,0 +1,75 @@
+.:
+total 8
+drwxr-xr-x 1 andros 197121 0 sep  6 13:44 ./
+rrwxr-xr-x 1 andros 197121 0 sep  6 13:42 ../
+drwxr-xr-x 1 andros 197121 0 sep  6 13:43 .git/
+-rw-r--r-- 1 andros 197121 0 sep  6 13:44 files.h
+
+./.git:
+total 11
+drwxr-xr-x 1 andros 197121   0 sep  6 13:43 ./
+drwxr-xr-x 1 andros 197121   0 sep  6 13:44 ../
+-rw-r--r-- 1 andros 197121 130 sep  6 13:43 config
+-rw-r--r-- 1 andros 197121  73 sep  6 13:43 description
+-rw-r--r-- 1 andros 197121  23 sep  6 13:43 HEAD
+drwxr-xr-x 1 andros 197121   0 sep  6 13:43 hooks/
+drwxr-xr-x 1 andros 197121   0 sep  6 13:43 info/
+drwxr-xr-x 1 andros 197121   0 sep  6 13:43 objects/
+drwxr-xr-x 1 andros 197121   0 sep  6 13:43 refs/
+
+./.git/hooks:
+total 49
+drwxr-xr-x 1 andros 197121    0 sep  6 13:43 ./
+drwxr-xr-x 1 andros 197121    0 sep  6 13:43 ../
+-rwxr-xr-x 1 andros 197121  478 sep  6 13:43 applypatch-msg.sample*
+-rwxr-xr-x 1 andros 197121  896 sep  6 13:43 commit-msg.sample*
+-rwxr-xr-x 1 andros 197121 4655 sep  6 13:43 fsmonitor-watchman.sample*
+-rwxr-xr-x 1 andros 197121  189 sep  6 13:43 post-update.sample*
+-rwxr-xr-x 1 andros 197121  424 sep  6 13:43 pre-applypatch.sample*
+-rwxr-xr-x 1 andros 197121 1643 sep  6 13:43 pre-commit.sample*
+-rwxr-xr-x 1 andros 197121  416 sep  6 13:43 pre-merge-commit.sample*
+-rwxr-xr-x 1 andros 197121 1492 sep  6 13:43 prepare-commit-msg.sample*
+-rwxr-xr-x 1 andros 197121 1348 sep  6 13:43 pre-push.sample*
+-rwxr-xr-x 1 andros 197121 4898 sep  6 13:43 pre-rebase.sample*
+-rwxr-xr-x 1 andros 197121  544 sep  6 13:43 pre-receive.sample*
+-rwxr-xr-x 1 andros 197121 3635 sep  6 13:43 update.sample*
+
+./.git/info:
+total 5
+drwxr-xr-x 1 andros 197121   0 sep  6 13:43 ./
+drwxr-xr-x 1 andros 197121   0 sep  6 13:43 ../
+-rw-r--r-- 1 andros 197121 240 sep  6 13:43 exclude
+
+./.git/objects:
+total 4
+drwxr-xr-x 1 andros 197121 0 sep  6 13:43 ./
+drwxr-xr-x 1 andros 197121 0 sep  6 13:43 ../
+drwxr-xr-x 1 andros 197121 0 sep  6 13:43 info/
+drwxr-xr-x 1 andros 197121 0 sep  6 13:43 pack/
+
+./.git/objects/info:
+total 0
+drwxr-xr-x 1 andros 197121 0 sep  6 13:43 ./
+drwxr-xr-x 1 andros 197121 0 sep  6 13:43 ../
+
+./.git/objects/pack:
+total 0
+drwxr-xr-x 1 andros 197121 0 sep  6 13:43 ./
+drwxr-xr-x 1 andros 197121 0 sep  6 13:43 ../
+
+./.git/refs:
+total 4
+drwxr-xr-x 1 andros 197121 0 sep  6 13:43 ./
+drwxr-xr-x 1 andros 197121 0 sep  6 13:43 ../
+drwxr-xr-x 1 andros 197121 0 sep  6 13:43 heads/
+drwxr-xr-x 1 andros 197121 0 sep  6 13:43 tags/
+
+./.git/refs/heads:
+total 0
+drwxr-xr-x 1 andros 197121 0 sep  6 13:43 ./
+drwxr-xr-x 1 andros 197121 0 sep  6 13:43 ../
+
+./.git/refs/tags:
+total 0
+drwxr-xr-x 1 andros 197121 0 sep  6 13:43 ./
+drwxr-xr-x 1 andros 197121 0 sep  6 13:43 ../
